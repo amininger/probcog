408730fbfe0f2eb5d015242f5d14008d08944c48
Robert T. Goeddel <rgoeddel@umich.edu>
2013-06-07 13:13:43 -0400

    Modified kinect framework with AprilCal plumbed in.

--- java/src/probcog/sensor/KinectSensor.java
+++ java/src/probcog/sensor/KinectSensor.java
@@ -27,11 +27,11 @@ public class KinectSensor
     // take some extra work to get to.
     //
     // parameters for IR depth camera
-    public static double Firx = 5.7191759217862204e+02; // focal length
-    public static double Firy = 5.8760489958891026e+02; //
+    //public static double Firx = 5.7191759217862204e+02; // focal length
+    //public static double Firy = 5.8760489958891026e+02; //
 
-    public static double Cirx = 3.37025048258259540e+02; // larger -> moves right
-    public static double Ciry = 2.4675008449138741e+02; // larger -> moves down
+    //public static double Cirx = 3.37025048258259540e+02; // larger -> moves right
+    //public static double Ciry = 2.4675008449138741e+02; // larger -> moves down
 
     //public static double Cirx = 3.2525048258259540e+02; // camera center in pixels
     //public static double Ciry = 2.4275008449138741e+02; //
@@ -42,25 +42,35 @@ public class KinectSensor
     kinect_status_t ks = null;
 
     // Calibration
-    Config calib;
+    Config color;
+    Config ir;
     View input;
     View output;
     Rasterizer rasterizer;
+
+    double Cirx, Ciry, Firx, Firy;
 
     // Stash
     kinect_status_t stash_ks;
     BufferedImage r_rgbIm;
     BufferedImage r_depthIm;
 
-    public KinectSensor(Config calib_) throws IOException
+    public KinectSensor(Config color_, Config ir_) throws IOException
     {
-        calib = calib_;
+        color = color_;
+        ir = ir_;
+
+        // Set IR Paremeters
+        Cirx = ir.requireDoubles("intrinsics.cc")[0];
+        Ciry = ir.requireDoubles("intrinsics.cc")[1];
+        Firx = ir.requireDoubles("intrinsics.fc")[0];
+        Firy = ir.requireDoubles("intrinsics.fc")[1];
 
         // Create the input view
         System.err.println("NFO: Initializing kinect calibration");
-        String classname = calib.requireString("class");
+        String classname = color.requireString("class");
 
-        Object obj = ReflectUtil.createObject(classname, calib);
+        Object obj = ReflectUtil.createObject(classname, color);
         assert (obj != null);
         assert (obj instanceof Calibration);
 
@@ -152,17 +162,41 @@ public class KinectSensor
                           ((stash_ks.rgb[i*3 + 1] & 0xff) << 8) |
                           ((stash_ks.rgb[i*3 + 2] & 0xff) << 16);
 
-                bdepth[i] = ((stash_ks.depth[i*2 + 0] * 0xff) << 0) |
-                            ((stash_ks.depth[i*2 + 1] * 0xff) << 8);
+                bdepth[i] = ((stash_ks.depth[i*2 + 0] & 0xff) << 0) |
+                            ((stash_ks.depth[i*2 + 1] & 0xff) << 8);
             }
         }
 
-        //r_rgbIm = rasterizer.rectifyImage(rgbIm);
-        //r_depthIm = rasterizer.rectifyImage(depthIm);
-        r_rgbIm = rgbIm;
-        r_depthIm = depthIm;
+        r_rgbIm = rasterizer.rectifyImage(rgbIm);
+        r_depthIm = rasterizer.rectifyImage(depthIm);
+        //r_rgbIm = rgbIm;
+        //r_depthIm = depthIm;
+        //
+        //System.out.printf("%d x %d -- %d x %d\n", r_rgbIm.getWidth(),
+        //                                          r_rgbIm.getHeight(),
+        //                                          r_depthIm.getWidth(),
+        //                                          r_depthIm.getHeight());
 
         return true;
+    }
+
+    /** Get the width of our rectified images */
+    public int getWidth()
+    {
+        if (r_rgbIm == null)
+            return 0;
+        assert (r_rgbIm.getWidth() == r_depthIm.getWidth());
+
+        return r_rgbIm.getWidth();
+    }
+
+    public int getHeight()
+    {
+        if (r_rgbIm == null)
+            return 0;
+        assert (r_rgbIm.getHeight() == r_depthIm.getHeight());
+
+        return r_rgbIm.getHeight();
     }
 
     // XXX Add bounds checking
@@ -223,7 +257,8 @@ public class KinectSensor
     {
         GetOpt opts = new GetOpt();
         opts.addBoolean('h',"help",false,"Show this help screen");
-        opts.addString('c',"config",null,"Calibration config");
+        opts.addString('c',"color",null,"RGB calibration config");
+        opts.addString('r',"ir",null,"IR calibration config");
 
         if (!opts.parse(args)) {
             System.err.println("ERR: Error parsing args. "+opts.getReason());
@@ -231,16 +266,21 @@ public class KinectSensor
         }
 
         if (opts.getBoolean("help") ||
-            opts.getString("config") == null)
+            opts.getString("color") == null ||
+            opts.getString("ir") == null)
         {
             opts.doHelp();
             System.exit(0);
         }
 
-        Config config = null;
+        Config color = null;
+        Config ir = null;
         try {
-            config = new ConfigFile(opts.getString("config"));
-            config = config.getChild("aprilCameraCalibration.camera0000");
+            color = new ConfigFile(opts.getString("color"));
+            color = color.getChild("aprilCameraCalibration.camera0000");
+
+            ir = new ConfigFile(opts.getString("ir"));
+            ir = ir.getChild("aprilCameraCalibration.camera0000");
         } catch (IOException ioex) {
             System.err.println("ERR: Could not open calibration config");
             ioex.printStackTrace();
@@ -262,7 +302,7 @@ public class KinectSensor
         KinectSensor kinect = null;
         try {
             // XXX HACK
-            kinect = new KinectSensor(config);
+            kinect = new KinectSensor(color, ir);
         } catch (IOException ioex) {
             System.err.println("ERR: Could not initialize KinectSensor");
             ioex.printStackTrace();
@@ -277,8 +317,8 @@ public class KinectSensor
 
             ArrayList<double[]> points = new ArrayList<double[]>();
             VisColorData vcd = new VisColorData();
-            for (int y = 0; y < kinect_status_t.HEIGHT; y++) {
-                for (int x = 0; x < kinect_status_t.WIDTH; x++) {
+            for (int y = 0; y < kinect.getHeight(); y++) {
+                for (int x = 0; x < kinect.getWidth(); x++) {
                     double[] xyz = kinect.getXYZ(x,y);
                     int rgb = kinect.getRGB(x,y);
 
