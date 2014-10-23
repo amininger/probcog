a02cfa972e47a3825d77a5ea3647c498d719d84e
Robert T. Goeddel <rgoeddel@umich.edu>
2014-10-23 18:32:46 -0400

    Sort of works.

--- java/src/probcog/slam/OfflineSLAM.java
+++ java/src/probcog/slam/OfflineSLAM.java
@@ -26,7 +26,12 @@ import magic2.lcmtypes.*;   // Must be magic2 for new tag_detections...
 public class OfflineSLAM
 {
     static final long STEP_TIME_US = 1*1000000;
-    static final double ODOM_NOISE = 0.1; // XXX No working directly from ticks?
+    static final double ODOM_ERR_DIST = 0.15;   // STDDEV of err/m traveled
+    static final double ODOM_ERR_DIST_FIXED = 0.010;
+    static final double ODOM_ERR_ROT = 0.100;   // STDDEV of err/rad
+    static final double ODOM_ERR_ROT_FIXED = 0.017;
+    static final double TAG_ERR_TRANS = 0.1;
+    static final double TAG_ERR_ROT = 100.0;
 
     // GUI Misc.
     VisWorld vw;
@@ -42,9 +47,13 @@ public class OfflineSLAM
     // XXX SYNCHRONIZATION IS AN ISSUE HERE...must examine LASER publishing code
     ArrayList<laser_t> lasers = new ArrayList<laser_t>();   // History of LASER?
     HashMap<Integer, Integer> tagIdxs = new HashMap<Integer, Integer>(); // In graph nodes
+    HashMap<Integer, Set<Integer> > tag2pose = new HashMap<Integer, Set<Integer> >();
     ArrayList<tag_detection_list_t> tags = new ArrayList<tag_detection_list_t>();
 
     // Graph state
+    boolean firstTime = true;
+    GraphSolver solver;
+    GraphSolver gssolver;
     Graph g;
 
     private class ButtonHandler implements ParameterListener
@@ -57,8 +66,15 @@ public class OfflineSLAM
             } else if (name.equals("run")) {
                 // Slurp up all of the log file
                 processLog(true);
+                postprocess();
             } else if (name.equals("opt")) {
                 // Optimize the graph
+                if (firstTime)
+                    for (int i = 0; i < 10000; i++)
+                        gssolver.iterate();
+                solver.iterate();
+                firstTime = false;
+                redraw();
             }
         }
     }
@@ -122,7 +138,8 @@ public class OfflineSLAM
         e.setAttribute("type", "odom");
         g.edges.add(e);
 
-        // Solver...probably Cholesky? XXX
+        solver = new CholeskySolver(g);
+        gssolver = new GaussSeidelSolver(g);
     }
 
     /** Process the log. If all == true, read in the entire log immediately.
@@ -156,6 +173,12 @@ public class OfflineSLAM
         } catch (IOException ex) {
             System.out.println("End of log reached.");
         }
+    }
+
+    /** Yeah. */
+    private void postprocess()
+    {
+        // XXX
     }
 
     private boolean handlePose(lcm.logging.Log.Event event) throws IOException
@@ -192,15 +215,15 @@ public class OfflineSLAM
         GXYTEdge e = new GXYTEdge();
         e.z = new double[] {dxyz[0],
                             dxyz[1],
-                            LinAlg.quatToRollPitchYaw(dq)[2]};
+                            MathUtil.mod2pi(LinAlg.quatToRollPitchYaw(dq)[2])};
         e.truth = null; // We don't know the truth
         e.setAttribute("type", "odom");
 
         // XXX Make this proportional eventually
-        double var = ODOM_NOISE*ODOM_NOISE;
-        e.P = new double[][] {{var, 0, 0},
-                              {0, var, 0},
-                              {0, 0, var}};
+        e.P = new double[3][3];
+        e.P[0][0] = 1.0 / LinAlg.sq(Math.abs(e.z[0]) * ODOM_ERR_DIST + ODOM_ERR_DIST_FIXED);
+        e.P[1][1] = 1.0 / LinAlg.sq(Math.abs(e.z[1]) * ODOM_ERR_DIST + ODOM_ERR_DIST_FIXED);
+        e.P[2][2] = 1.0 / LinAlg.sq(Math.abs(e.z[2]) * ODOM_ERR_ROT + ODOM_ERR_ROT_FIXED);
         e.nodes = new int[] {poseIdxs.get(poseIdxs.size()-1), g.nodes.size()};
         poseIdxs.add(g.nodes.size());
 
@@ -255,8 +278,35 @@ public class OfflineSLAM
                 g.nodes.add(n);
             }
 
-            // Associate this tag with a robot pose node, if appropriate by
-            // creating an edge between them
+            // Make an edge if applicable
+            if (!tag2pose.containsKey(td.id))
+                tag2pose.put(td.id, new HashSet<Integer>());
+            if (!tag2pose.get(td.id).contains(poseIdxs.get(poseIdxs.size()-1))) {
+                GXYTEdge e = new GXYTEdge();
+
+                double[][] T = LinAlg.matrixAB(T_p, T_h);
+
+                e.z = new double[] {T[0][3],
+                                    T[1][3],
+                                    MathUtil.mod2pi(LinAlg.matrixToRollPitchYaw(T)[2])};
+                e.truth = null; // We don't know the truth.
+                // You can't handle the truth!
+
+                // XXX This will need to change
+                e.P = new double[3][3];
+                e.P[0][0] = TAG_ERR_TRANS;
+                e.P[1][1] = TAG_ERR_TRANS;
+                e.P[2][2] = TAG_ERR_ROT;
+                e.nodes = new int[] {poseIdxs.get(poseIdxs.size()-1),
+                                     tagIdxs.get(td.id)};
+                e.setAttribute("type", "tag");
+                e.setAttribute("id", new Integer(td.id));
+
+                tag2pose.get(td.id).add(poseIdxs.get(poseIdxs.size()-1));
+                g.edges.add(e);
+            }
+
+
          }
     }
 
@@ -279,10 +329,6 @@ public class OfflineSLAM
         M = CameraUtil.scalePose(M, 2.0, -tagSize);
         M = LinAlg.matrixAB(LinAlg.rotateZ(-Math.PI/2), M);
 
-        // Now, convert to a RBT that relates robot to tag
-        //double[] xyt = LinAlg.matrixToXYT(M);
-        //LinAlg.print(xyt);
-
         return M;
     }
 
@@ -297,10 +343,13 @@ public class OfflineSLAM
         vbTags.setDrawOrder(10);
         VisWorld.Buffer vbRobots = vw.getBuffer("poses");
         vbRobots.setDrawOrder(0);
+        VisWorld.Buffer vbTagObs= vw.getBuffer("tag-obs");
+        vbTagObs.setDrawOrder(-5);
         VisWorld.Buffer vbTraj = vw.getBuffer("trajectory");
         vbTraj.setDrawOrder(-10);
 
         ArrayList<double[]> lines = new ArrayList<double[]>();
+        ArrayList<double[]> taglines = new ArrayList<double[]>();
 
         for (GEdge o: g.edges) {
             if (o instanceof GXYTEdge) {
@@ -313,17 +362,24 @@ public class OfflineSLAM
                 GXYTNode a = (GXYTNode) g.nodes.get(e.nodes[0]);
                 GXYTNode b = (GXYTNode) g.nodes.get(e.nodes[1]);
 
-                if (type == null)
+                if (type == null) {
                     continue;
-                else if (type.equals("odom")) {
+                } else if (type.equals("odom")) {
                     lines.add(LinAlg.resize(a.state, 2));
                     lines.add(LinAlg.resize(b.state, 2));
+                } else if (type.equals("tag")) {
+                    taglines.add(LinAlg.resize(a.state, 2));
+                    taglines.add(LinAlg.resize(b.state, 2));
                 }
             }
         }
         vbTraj.addBack(new VzLines(new VisVertexData(lines),
                                    VzLines.LINES,
                                    new VzLines.Style(Color.blue, 2)));
+        vbTagObs.addBack(new VzLines(new VisVertexData(taglines),
+                                     VzLines.LINES,
+                                     new VzLines.Style(Color.magenta, 2)));
+
 
         for (GNode o: g.nodes) {
             if (o instanceof GXYTNode) {
@@ -340,7 +396,7 @@ public class OfflineSLAM
                     vbTags.addBack(new VisChain(LinAlg.xytToMatrix(n.state),
                                                 new VzCircle(0.075,
                                                              new VzMesh.Style(Color.red)),
-                                                LinAlg.scale(0.005),
+                                                LinAlg.scale(0.1),
                                                 new VzText(VzText.ANCHOR.CENTER, f)));
                 }
             }
@@ -348,6 +404,7 @@ public class OfflineSLAM
 
         vbTags.swap();
         vbRobots.swap();
+        vbTagObs.swap();
         vbTraj.swap();
     }
 
