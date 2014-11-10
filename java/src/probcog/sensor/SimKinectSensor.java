19def9389555526f1d7176f8dc5269b8c7525646
Aaron Mininger <mininger@umich.edu>
2014-11-10 10:33:03 -0500

    tweaked some world files

--- java/src/probcog/sensor/SimKinectSensor.java
+++ java/src/probcog/sensor/SimKinectSensor.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
+import probcog.sim.SimFlatSurface;
 import probcog.sim.SimLocation;
 import probcog.sim.SimObjectPC;
 import april.jmat.LinAlg;
@@ -286,6 +287,9 @@ public class SimKinectSensor implements Sensor
     	if(pixels[i].target == null){
     		// Ray didn't hit anything
     		//pixels[i].point = new double[4];
+    		return false;
+    	} else if(pixels[i].target instanceof SimFlatSurface){
+    		pixels[i].target = null;
     		return false;
     	}
 
