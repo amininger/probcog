8e3e17ede949b266c5d0a5e48fcdc908fef6d46f
Robert T. Goeddel <rgoeddel@umich.edu>
2014-11-10 17:40:47 -0500

    WIP on swapping control law interface.

---
+++ java/src/probcog/commands/controls/DriveParams.java
@@ -0,0 +1,21 @@
+package probcog.commands.controls;
+
+import probcog.lcmtypes.*;
+
+/** A container class holding possible inputs to a control law.
+ *  It is expected that these will be a fairly small subset of
+ *  things, but this may eventually grow bloated, at which point
+ *  reevaluation should be considered.
+ **/
+public class DriveParams
+{
+    public classification_t classy; // XXX
+    public laser_t laser;
+    public pose_t pose;
+    public double dt;
+
+    public DriveParams()
+    {
+
+    }
+}
