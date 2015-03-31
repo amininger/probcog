04b9d7b8119e80ff460a7bb17d3d0061e3be793f
Robert T. Goeddel <rgoeddel@umich.edu>
2015-03-31 16:46:30 -0400

    Basic obstacle potentials implemented.

--- java/src/probcog/commands/controls/PotentialUtil.java
+++ java/src/probcog/commands/controls/PotentialUtil.java
@@ -37,7 +37,7 @@ public class PotentialUtil
 
         // Is this value set correctly in config? Reevaluate for new robot.
         public double robotRadius = Util.getConfig().requireDouble("robot.geometry.radius");
-        public double fieldSize = 5.0;  // [meters];
+        public double fieldSize = 10.0;  // [meters];
         public double fieldRes = 0.1;   // [meters / pixel]
 
         // attractiveThreshold used for combined method, specifying a distance
@@ -45,6 +45,10 @@ public class PotentialUtil
         public AttractivePotential attractivePotential = AttractivePotential.LINEAR;
         public double attractiveWeight = 1.0;
         public double attractiveThreshold = 1.0;
+
+        //
+        public double repulsiveWeight = 100.0;
+        public double maxObstacleRange = 3*robotRadius; // XXX
     }
 
     /** Given application specific parameters, generate a potential field
@@ -59,12 +63,18 @@ public class PotentialUtil
                                                params.fieldSize,
                                                params.fieldRes);
 
-        getAttractivePotential(params, pf);
+        addAttractivePotential(params, pf);
+        addRepulsivePotential(params, pf);
 
         return pf;
     }
 
-    static private void getAttractivePotential(Params params,
+    /** Add attrative potential to the system in one of three forms.
+     *  1) Linear/conical potential. Directly proportional to distance to goal.
+     *  2) Quadratic potential. Square of distance to goal.
+     *  3) Combined potential. Starts quadratic, but linear beyond some point.
+     **/
+    static private void addAttractivePotential(Params params,
                                                PotentialField pf)
     {
         double kw = params.attractiveWeight;
@@ -96,6 +106,54 @@ public class PotentialUtil
         }
     }
 
+    /** Generate repulsive potential based on obstacles observed near the robot.
+     *  Derives these range measurements from the provided laser_t in params.
+     **/
+    static private void addRepulsivePotential(Params params,
+                                              PotentialField pf)
+    {
+        double kw = params.repulsiveWeight;
+        double kr = params.maxObstacleRange;
+        double invKr = 1.0/kr;
+
+        double[] xyt = LinAlg.matrixToXYT(LinAlg.quatPosToMatrix(params.pose.orientation,
+                                                                 params.pose.pos));
+        double[] invXyt = LinAlg.xytInverse(xyt);
+
+        // Convert laser_t measurements to global coordinates. Ignore ranges
+        // that cannot contribute to our potential.
+        ArrayList<double[]> points = new ArrayList<double[]>();
+        for (int i = 0; i < params.laser.nranges; i++) {
+            double r = params.laser.ranges[i];
+
+            // Error value
+            if (r < 0)
+                continue;
+            // Too far away to contribute potential
+            if (r > kr)
+                continue;
+            double t = params.laser.rad0 + i*params.laser.radstep;
+            double[] xy = new double[] { r*Math.cos(t), r*Math.sin(t) };
+            points.add(LinAlg.transform(xyt, xy));
+        }
+
+        // Determine the distance to the closest point
+        for (int y = 0; y < pf.getHeight(); y++) {
+            for (int x = 0; x < pf.getWidth(); x++) {
+                double[] xy = pf.indexToMeters(x, y);
+                double d = Double.MAX_VALUE;
+                for (double[] pxy: points) {
+                    d = Math.min(d, LinAlg.distance(xy, pxy, 2));
+                }
+
+                // No potential added
+                if (d > kr)
+                    continue;
+                pf.addIndex(x, y, 0.5*kw*LinAlg.sq(invKr - d));
+            }
+        }
+    }
+
     static public void main(String[] args)
     {
         double[] goal = new double[] {4, 2, 0};
@@ -103,7 +161,26 @@ public class PotentialUtil
         pose.orientation = new double[] {1, 0, 0, 0};
         pose.pos = new double[] {1.5, 1, 0};
 
-        Params params = new Params(new laser_t(), pose, goal);
+        // Fake a hallway. Wall on right is 1m away, wall on left is 0.5m
+        laser_t laser = new laser_t();
+        laser.rad0 = (float)(-Math.PI/2);
+        laser.radstep = (float)(Math.toRadians(1));
+        laser.nranges = 180;
+        laser.ranges = new float[laser.nranges];
+        for (int i = 0; i < laser.nranges; i++) {
+            double t = laser.rad0 + i*laser.radstep;
+            double r = -1;
+            if (t < 0) {
+                r = -0.5/Math.sin(t);
+            } else if (t > 0) {
+                r = 1.0/Math.sin(t);
+            }
+            if (r > 30 || r < 0)
+                r = -1;
+            laser.ranges[i] = (float)r;
+        }
+
+        Params params = new Params(laser, pose, goal);
         params.attractivePotential = AttractivePotential.COMBINED;
         PotentialField pf = PotentialUtil.getPotential(params);
 
