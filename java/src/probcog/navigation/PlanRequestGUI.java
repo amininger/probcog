223f761db1a3b36eb314541eba7b848d8de7b35b
Robert T. Goeddel <rgoeddel@umich.edu>
2015-07-27 14:27:29 -0400

    Can now request plans of robot.

--- java/src/probcog/navigation/PlanRequestGUI.java
+++ java/src/probcog/navigation/PlanRequestGUI.java
@@ -8,6 +8,7 @@ import java.util.*;
 import lcm.lcm.*;
 
 import april.config.*;
+import april.jmat.*;
 import april.util.*;
 import april.sim.*;
 
@@ -17,13 +18,68 @@ import probcog.sim.*;
 /** A test GUI for generating plan requests. */
 public class PlanRequestGUI
 {
+    static final int REQUEST_HZ = 5;
+
+    LCM lcm = LCM.getSingleton();
+    String requestChannel;
+    Object requestLock = new Object();
+    plan_request_t request = null;
+
     SimWorld world = null;
+
+    private class LCMThread extends Thread
+    {
+        public void run()
+        {
+            while (true) {
+                TimeUtil.sleep(1000/REQUEST_HZ);
+
+                if (request == null)
+                    continue;
+                request.utime = TimeUtil.utime();
+                lcm.publish(requestChannel, request);
+            }
+        }
+    }
+
+    private class PGHandler implements ParameterListener
+    {
+        byte id = 0;
+        double[] xyt = new double[3];
+
+        public void parameterChanged(ParameterGUI pg, String name)
+        {
+            if (world != null && name.equals("tag")) {
+                for (SimObject so: world.objects) {
+                    if (!(so instanceof SimAprilTag))
+                        continue;
+                    SimAprilTag tag = (SimAprilTag)so;
+                    if (tag.getID() != pg.gi("tag"))
+                        continue;
+                    xyt = LinAlg.matrixToXYT(tag.getPose());
+                }
+            } else if (name.equals("x") || name.equals("y")) {
+                xyt[0] = pg.gd("x");
+                xyt[1] = pg.gd("y");
+                xyt[2] = 0;
+            } else if (name.equals("send")) {
+                // Issue a plan request
+                synchronized (requestLock) {
+                    request = new plan_request_t();
+                    request.id = id++;
+                    request.goalXYT = LinAlg.copy(xyt);
+                }
+            }
+        }
+    }
 
     public PlanRequestGUI(GetOpt gopt) throws IOException
     {
         JFrame jf = new JFrame("Plan Request Test GUI");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setLayout(new BorderLayout());
+
+        requestChannel = gopt.getString("request-channel");
 
         if (gopt.wasSpecified("world")) {
             Config config = new Config();
@@ -38,7 +94,10 @@ public class PlanRequestGUI
             pg.addInt("tag", "Tag #", 0);
         }
         pg.addButtons("send", "Send");
+        pg.addListener(new PGHandler());
         jf.add(pg, BorderLayout.CENTER);
+
+        new LCMThread().start();
 
         jf.setMinimumSize(new Dimension(400, 0));
         jf.pack();
@@ -47,18 +106,19 @@ public class PlanRequestGUI
 
     static public void main(String[] args)
     {
-        GetOpt opts = new GetOpt();
-        opts.addBoolean('h', "help", false, "Show this help screen");
-        opts.addString('w', "world", null, "Optional sim world w/tag locations");
+        GetOpt gopt = new GetOpt();
+        gopt.addBoolean('h', "help", false, "Show this help screen");
+        gopt.addString('w', "world", null, "Optional sim world w/tag locations");
+        gopt.addString('\0', "request-channel", "PLAN_REQUEST", "Plan request LCM channel");
 
-        if (!opts.parse(args) || opts.getBoolean("help")) {
+        if (!gopt.parse(args) || gopt.getBoolean("help")) {
             System.err.printf("Usage: %s <options>\n", args[0]);
-            opts.doHelp();
+            gopt.doHelp();
             System.exit(1);
         }
 
         try {
-            new PlanRequestGUI(opts);
+            new PlanRequestGUI(gopt);
         } catch (IOException ex) {
             System.err.println("ERR: "+ex);
             ex.printStackTrace();
